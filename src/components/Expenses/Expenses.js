64fc17b22f6c048e2fdc366d9c0a941347ab513e
Kaela's Workstation <mac3@KaelasWkstation.hiteccopy.local>
2021-09-02 14:29:53 -0500

    Section 5 Complete

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
-import "./Expenses.css";
+import "./Expenses.css"
 import ExpensesList from './ExpensesList'
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -24,6 +25,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
+        <ExpensesChart expenses ={filteredExpenses}/>
        <ExpensesList items = {filteredExpenses}/>
 
       </Card>
